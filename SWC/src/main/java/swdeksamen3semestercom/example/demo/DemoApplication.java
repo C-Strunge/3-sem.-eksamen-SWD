c228c73038aa2be9d9c7f23fd8b7807313b6db86
C-Strunge <33119378+C-Strunge@users.noreply.github.com>
2018-11-02 11:28:34 +0100

    Codinstandards init.

---
+++ SWC/src/main/java/swdeksamen3semestercom/example/demo/DemoApplication.java
@@ -0,0 +1,12 @@
+package swdeksamen3semestercom.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
