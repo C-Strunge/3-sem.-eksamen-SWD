bcea7df0f5f44d4eb6b8ffddbaf814a69951e2c5
MatthiasSkou <ms@klinikskou.dk>
2018-12-05 12:02:57 +0100

    editCompany US01.6 tested & done

--- thenext/src/main/java/mandatory/two/controller/CompanyController.java
+++ thenext/src/main/java/mandatory/two/controller/CompanyController.java
@@ -6,12 +6,10 @@ import mandatory.two.repository.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 /**
  * Created by Matthias Skou 30/11/2018
@@ -44,14 +42,20 @@ public class CompanyController {
 
     @GetMapping("/company/edit/{id}")
     public String editCompany(@PathVariable Long id, Model model){
+        Optional<Company> company = companyRepo.findById(id);
+        Company c = company.get();
+        System.out.println("ID BEFORE: " + c.getId());
+        c.setId(id);
         model.addAttribute("category", categoryRepo.findAll());
-        model.addAttribute("company", companyRepo.findById(id));
-
+        model.addAttribute("company", c);
         return "editCompany";
     }
 
     @PostMapping("/company/edit")
     public String editCompany(@ModelAttribute Company company){
+        System.out.println(company.getId());
+        System.out.println(company.getCompanyName());
+        System.out.println(company.getFirstName());
             companyRepo.save(company);
         return "";
     }
